f2e502c2a03cd1316f785423254d37657cf10fd3
M Yang <miloyang9@gmail.com>
2023-11-07 11:55:27 +1100

    updated seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department (id, name)
+VALUES (2, "Engineering"),
+       (3, "Finance"),
+       (4, "Legal"),
+       (1, "Sales");
+
+INSERT INTO role (title, department_id, salary)
+VALUES ("Sales Lead", 1, 100000),
+       ("Salesperson", 1, 80000),
+       ("Lead Engineer", 2, 150000),
+       ("Software Engineer", 2, 120000),
+       ("Account Manager", 3, 160000),
+       ("Accountant", 3, 125000),
+       ("Legal Team Lead", 4, 250000),
+       ("Lawyer", 4, 190000);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Smith", 1),
+       ("Michael", "Johnson", 2, 1),
+       ("Ashley", "Rodriguez", 3),
+       ("Kevin", "Tupik", 4, 2),
+       ("Kunal", "Singh", 5),
+       ("Malia", "Brown", 6, 3),
+       ("Sarah", "Lourd", 7),
+       ("Tom", "Allen", 8, 4);
